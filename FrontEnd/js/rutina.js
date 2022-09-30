589121e8195c3863f2c70947c7b2e72ab0d50472
Alexis Chavarria <alexischavarria@hotmail.com>
2022-09-30 17:56:07 -0500

    F

--- FrontEnd/js/rutina.js
+++ FrontEnd/js/rutina.js
@@ -1,3 +1,45 @@
 let listaRutinas = [
-    
+    {id: "1", nombre: "Rutina principiantes",categoria:"Hipertrofia",duracion:"24 dias",intensidad:"media",cantidadEjercicios:"12"},
+    {id: "2", nombre: "Rutina intermedia",categoria:"Hipertrofia",duracion:"30 dias",intensidad:"Alta",cantidadEjercicios:"18"},
+    {id: "3", nombre: "Rutina principiantes",categoria:"Cardio",duracion:"24 dias",intensidad:"media",cantidadEjercicios:"12"},
+    {id: "4", nombre: "Rutina intermedia",categoria:"Cardio",duracion:"30 dias",intensidad:"Alta",cantidadEjercicios:"18"},
+    {id: "5", nombre: "Rutina principiantes",categoria:"Fuerza",duracion:"24 dias",intensidad:"media",cantidadEjercicios:"12"},
+    {id: "6", nombre: "Rutina intermedia",categoria:"Fuerza",duracion:"30 dias",intensidad:"Alta",cantidadEjercicios:"18"}
 ];
+
+function rellenarTabla(){
+    const tabla = document.getElementById('rutinas')
+    let tbody = ''
+    for (let i = 0; i < listaRutinas.length; i++) {
+        const rutina = listaRutinas[i]
+        tbody += `
+        <tr>
+            <td>${rutina.id}</td>
+            <td>${rutina.nombre}</td>
+            <td>${rutina.categoria}</td>
+            <td>${rutina.duracion}</td>
+            <td>${rutina.intensidad}</td>
+            <td>${rutina.cantidadEjercicios}</td>
+            <td> 
+                <button class="btn btn-warning" onclick='actualizar(${JSON.stringify(rutina)})'>Actualizar</button> 
+                <button class="btn btn-danger" onclick='eliminarRutina(${rutina.id})'>X</button>
+            </td>
+        </tr>
+        `
+        }
+        // tbody += ''
+        tabla.innerHTML = tbody;
+}
+
+function main(){
+    rellenarTabla()
+}
+main()
+
+
+function nuevaRutina(){
+
+    const tabla = document.querySelector(".no-mostrar");
+    /* tabla.classList.remove('.no-mostrar'); */
+    tabla.style.display = "inline-table";
+}
